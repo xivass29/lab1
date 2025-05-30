d5729c47a659bc268a4a141a8e1768726665c3b2
стук валера <stuk.nicit@mail.ru>
2025-05-30 09:04:07 +0300

    fh

---
+++ src/main/java/com/example/javalabaip/service/IpLocationService.java
@@ -0,0 +1,45 @@
+package com.example.javalabaip.service;
+
+import com.example.javalabaip.dto.LocationResponseDto;
+import com.example.javalabaip.model.Location;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.server.ResponseStatusException;
+
+@Service
+public class IpLocationService {
+
+    private final RestTemplate restTemplate;
+
+    public IpLocationService(RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
+    }
+
+    public LocationResponseDto getLocationByIp(String ipAddress) {
+        try {
+
+            String apiUrl = "http://ip-api.com/json/" + ipAddress;
+            Location location = restTemplate.getForObject(apiUrl, Location.class);
+
+            if (location == null || location.getCity() == null || location.getCountry() == null) {
+                throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Неверный IP-адрес или ошибка API");
+            }
+
+            location.setIpAddress(ipAddress);
+
+            LocationResponseDto response = new LocationResponseDto();
+            response.setIpAddress(location.getIpAddress());
+            response.setCity(location.getCity());
+            response.setCountry(location.getCountry());
+            response.setContinent(location.getContinent());
+
+            return response;
+        } catch (HttpClientErrorException e) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Неверный IP-адрес: " + ipAddress, e);
+        } catch (Exception e) {
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "Ошибка получения данных о местоположении", e);
+        }
+    }
+}
